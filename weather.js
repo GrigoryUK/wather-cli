f89d3b2b369d1aab592e6be74b879781f3c6168f
GrigoryUK <g.usoltsev.worksrocket@gmail.com>
2023-04-09 16:31:26 +0500

    библиотека chalk - чтобы красить log библиотека dedent - убирает переносы

--- weather.js
+++ weather.js
@@ -1,8 +1,21 @@
-import {getArgs} from './helpers/args'
-
+#!/usr/bin/env node
+import { getArgs } from './helpers/args.js'
+import { printError, printHelp, printSuccess } from './services/log.service.js'
 const initCLI = () => {
 	const args = getArgs(process.argv); 
 	console.log(args);
+	if (args.h) {
+		printHelp();
+	}
+	if (args.s) {
+
+	}
+	if (args.t) {
+
+	}
 };
 
 initCLI()
+
+printError('helpers')
+printSuccess('helpers')
